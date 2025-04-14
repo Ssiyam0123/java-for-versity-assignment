404e0e419e61da798e601febf8a8c6575d1c3916
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-14 15:39:48 +0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        TeddyBear bear1 = new TeddyBear(); // uses class from TeddyBear.java
+        bear1.showColor();
+    }
+}
---
+++ src/TeddyBear.java
@@ -0,0 +1,12 @@
+public class TeddyBear {
+    String color;
+
+    public TeddyBear(){
+        color="Red";
+        System.out.println("New teddy bear generated");
+    }
+
+    public void showColor(){
+        System.out.println("Ei bear ta holo "+ color);
+    }
+}
