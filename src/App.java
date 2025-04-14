6f9888bde09329714a984c9f95e21d4a50a3e78c
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-14 15:59:19 +0600

    simple sum calculaotr by java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        TeddyBear bear1 = new TeddyBear(); // uses class from TeddyBear.java
-        bear1.showColor();
+        // TeddyBear bear1 = new TeddyBear(); // If needed
+        Calc forSum = new Calc();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        float a = input.nextFloat();
+
+        System.out.print("Enter the second number: ");
+        float b = input.nextFloat();
+
+        float resultForSum = forSum.sum(a, b); // Use float
+        System.out.println("Result: " + resultForSum);
+
+        input.close();
     }
 }
---
+++ src/Calc.java
@@ -0,0 +1,5 @@
+public class Calc {
+    public float sum(float num1, float num2) {
+        return num1 + num2;
+    }
+}
